8c2c532d6395db0df9b7eba6cfc7a19059ff6fcc
unknown <prodby.vriel@gmail.com>
2020-10-19 23:56:19 +0200

    cat select

--- src/components/Filters/CatSelect/CatSelect.js
+++ src/components/Filters/CatSelect/CatSelect.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
-import { Field, Formik } from 'formik';
+import { Field, Formik, Form } from 'formik';
 
-const CatSelect = () => {
-   const categories = [];
+const CatSelect = ({ config }) => {
+   const [showAll, setShowAll] = useState(config.displayed);
+   const initialValues = {
+      [config.dataName]: config.data[0].name,
+   };
    return (
       <div className='catSelect-container'>
-         <h2>Categories</h2>
+         <h2>{config.label}</h2>
+         <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+               alert(JSON.stringify(values));
+            }}
+         >
+            {({ values }) => (
+               <Form>
+                  <ul>
+                     {config.data
+                        .slice(0, showAll)
+                        .map(({ displaName, name }, i) => (
+                           <li>
+                              <Field
+                                 type='radio'
+                                 id={name}
+                                 name={config.dataName}
+                                 value={name}
+                              />
+                              <label htmlFor={name}>{displaName}</label>
 
-         <ul>
-            <li>
-               <input type='radio' id='r-option' name='selector' />
-               <label htmlFor='r-option'>All Genre</label>
-
-               <div class='check'></div>
-            </li>
-            <li>
-               <input type='radio' id='f-option' name='selector' />
-               <label htmlFor='f-option'>Hip Hop</label>
-
-               <div class='check'></div>
-            </li>
-
-            <li>
-               <input type='radio' id='s-option' name='selector' />
-               <label htmlFor='s-option'>Cinematic</label>
-
-               <div class='check'>
-                  <div class='inside'></div>
-               </div>
-            </li>
-
-            <li>
-               <input type='radio' id='t-option' name='selector' />
-               <label htmlFor='t-option'>Techno</label>
-
-               <div class='check'>
-                  <div class='inside'></div>
-               </div>
-            </li>
-         </ul>
-         <p className='show-more'>+ Show more 4</p>
-         <div className='divider'></div>
+                              <div class='check'></div>
+                           </li>
+                        ))}
+                  </ul>
+                  {showAll < config.data.length && (
+                     <p
+                        className='show-more'
+                        onClick={() => setShowAll(config.data.length)}
+                     >
+                        + Show more {config.data.length - config.displayed}
+                     </p>
+                  )}
+                  <div>{values.categories}</div>
+                  <div className='divider'></div>
+               </Form>
+            )}
+         </Formik>
       </div>
    );
 };
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -6,13 +6,49 @@ import { Route, Switch } from 'react-router-dom';
 import LoopsPage from '../../pages/LoopsPage/LoopsPage';
 import CatSelect from '../../components/Filters/CatSelect/CatSelect';
 
+const config = {
+   label: 'Categories',
+   dataName: 'categories',
+   displayed: 4,
+   data: [
+      {
+         displaName: 'All Genre',
+         name: 'all-genre',
+      },
+      {
+         displaName: 'Hip Hop',
+         name: 'hip-hop',
+      },
+      {
+         displaName: 'Cinematics',
+         name: 'cinematics',
+      },
+      {
+         displaName: 'Techno',
+         name: 'techno',
+      },
+      {
+         displaName: 'Techno',
+         name: 'ts',
+      },
+      {
+         displaName: 'Techno',
+         name: 'tg',
+      },
+      {
+         displaName: 'Techno',
+         name: 'ty',
+      },
+   ],
+};
+
 const Dashboard = () => {
    return (
       <div className='dashboard-container page-container'>
          <div className='dashboard-wrapper'>
             <div className='dashboard-left'>
                <div className='filters'>
-                  <CatSelect />
+                  <CatSelect config={config} />
                </div>
             </div>
             <div className='dashboard-right'>
